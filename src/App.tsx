a4a9523009f4e064998ee7f902abcccd22a41f6b
Clement Farabet <clmt@google.com>
2025-01-19 17:21:08 -0500

    Forking to create an app for kids

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ import { useRef, useState } from "react";
 import "./App.scss";
 import { LiveAPIProvider } from "./contexts/LiveAPIContext";
 import SidePanel from "./components/side-panel/SidePanel";
-import { Altair } from "./components/altair/Altair";
+import { DrawStory } from "./components/draw-story/DrawStory";
 import ControlTray from "./components/control-tray/ControlTray";
 import cn from "classnames";
 
@@ -45,7 +45,7 @@ function App() {
           <main>
             <div className="main-app-area">
               {/* APP goes here */}
-              <Altair />
+              <DrawStory />
               <video
                 className={cn("stream", {
                   hidden: !videoRef.current || !videoStream,
